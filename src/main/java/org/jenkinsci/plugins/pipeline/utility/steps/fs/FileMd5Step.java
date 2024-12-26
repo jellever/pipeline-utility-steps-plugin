4ae28885e8c0be510caf7f0d7478c8e59538e341
Jelle Vergeer <jellever@users.noreply.github.com>
2024-12-27 00:48:31 +0100

    Create FileMd5Step.java

---
+++ src/main/java/org/jenkinsci/plugins/pipeline/utility/steps/fs/FileMd5Step.java
@@ -0,0 +1,50 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Emanuele Zattin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.jenkinsci.plugins.pipeline.utility.steps.fs;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+
+/**
+ * Compute the MD5 of a file.
+ *
+ * @author Emanuele Zattin &lt;emanuelez@gmail.com&gt;.
+ */
+public class FileMd5Step extends FileHashStep {
+    @DataBoundConstructor
+    public FileMd5Step(String file) throws Descriptor.FormException {
+        super(file, "MD5");
+    }
+
+
+    @Extension
+    public static class DescriptorImpl extends FileHashStep.DescriptorImpl {
+        public DescriptorImpl() {
+            super("MD5");
+        }
+    }
+}
